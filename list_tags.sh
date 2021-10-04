1e920f305d155affb0ca3f171fd4c303edcb2fa5
Maxym Zalata <zalatik@gmail.com>
2021-10-04 12:13:27 +0300

    choice as string

--- list_tags.sh
+++ list_tags.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws ecr list-images --repository-name petclinic --output text | awk '{print $3}'
+aws ecr list-images --repository-name petclinic --output text | awk '{print $3}' | sort -r | paste -sd ' '
